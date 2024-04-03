000f91ece579645a74d5cbd19645762d1281d8dc
Tim Habersack <tim.habersack@gmail.com>
2024-04-03 12:26:45 +0100

    no sleeping

--- www/index.php
+++ www/index.php
@@ -26,13 +26,13 @@ $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
 switch ($routeInfo[0]) {
     case \FastRoute\Dispatcher::NOT_FOUND:
         // ... 404 Not Found
-        sleep(5);
+        //sleep(5);
         header("HTTP/1.0 404 Not Found");
         break;
     case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
         $allowedMethods = $routeInfo[1];
         // ... 405 Method Not Allowed
-        sleep(5);
+        //sleep(5);
         header("HTTP/1.0 405 Method Not Allowed");
         break;
     case \FastRoute\Dispatcher::FOUND:
